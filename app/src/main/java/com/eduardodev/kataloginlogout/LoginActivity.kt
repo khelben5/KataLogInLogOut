fc3aeb74a4195db906778657820024c47a55567d
Eduardo Pascua <pascua.eduardo@gmail.com>
2019-01-24 16:39:23 +0100

    Add add login functionality

--- app/src/main/java/com/eduardodev/kataloginlogout/LoginActivity.kt
+++ app/src/main/java/com/eduardodev/kataloginlogout/LoginActivity.kt
@@ -2,11 +2,29 @@ package com.eduardodev.kataloginlogout
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        setupButtonListener()
+    }
+
+    private fun setupButtonListener() {
+        loginButton.setOnClickListener { checkForm() }
+    }
+
+    private fun checkForm() {
+        val userNameText = userName.text.toString()
+        val passwordText = password.text.toString()
+
+        if (userNameText == "admin" && passwordText == "admin") {
+            Toast.makeText(this, "OK", Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(this, "ERROR", Toast.LENGTH_LONG).show()
+        }
     }
 }
